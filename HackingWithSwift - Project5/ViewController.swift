721184fb17f038fa126439a352daab6d420c0b9d
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-16 16:01:20 -0600

    Call to text file, word shuffle, and presentment of first in the word array.

--- HackingWithSwift - Project5/ViewController.swift
+++ HackingWithSwift - Project5/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import GameplayKit
+
+var allWords = [String]()
+var usedWords = [String]()
+
+
 
 class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
