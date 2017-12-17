33c3d410cc7b8b5f365b9a1adc0dfeffdac21ffd
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-16 23:15:02 -0600

    Large code reorg, many errors fixed.

--- HackingWithSwift - Project5/ViewController.swift
+++ HackingWithSwift - Project5/ViewController.swift
@@ -19,21 +19,6 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        @objc func promptForAnswer() {
-            let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
-            ac.addTextField()
-            
-            let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
-                let answer = ac.testFields![0]
-                self.submit(answer : answer.text!)
-            }
-            
-            ac.addAction(submitAction)
-            present(ac, animated: true)
-            
-        }
-        
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
@@ -45,15 +30,10 @@ class ViewController: UITableViewController {
         }
         
         startGame()
-    }
-    
-    func startGame() {
-        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
-        title = allWords[0]
-        usedWords.removeAll(keepingCapacity: true)
-        tableView.reloadData()
+
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
@@ -63,13 +43,64 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
+
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+            let answer = ac.textFields![0]
+            self.submit(answer : answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+
+    func isPossible(word:String) -> Bool {
+        return true
+    }
+
+    func isOriginal(word:String) -> Bool {
+        return true
+    }
+
+    func isReal(word:String) -> Bool {
+        return true
+    }
+
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            
+            if isOriginal(word: lowerAnswer) {
+                
+                if isReal(word: lowerAnswer) {
+                    
+                    usedWords.insert(answer, at:0)
+                    let indexPath = IndexPath(row:0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                }
+            }
+        }
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-
 }
+
 
