37dc4b40a59a6cbe45c69010e52b53fdb08d856d
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-16 22:43:03 -0600

    Action closure added, open errors remain for resolution.

--- HackingWithSwift - Project5/ViewController.swift
+++ HackingWithSwift - Project5/ViewController.swift
@@ -19,6 +19,23 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        @objc func promptForAnswer() {
+            let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            
+            let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+                let answer = ac.testFields![0]
+                self.submit(answer : answer.text!)
+            }
+            
+            ac.addAction(submitAction)
+            present(ac, animated: true)
+            
+        }
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
             if let startWords = try? String(contentsOfFile: startWordsPath) {
                 allWords = startWords.components(separatedBy: "\n")
